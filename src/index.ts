cbf90076872ebf97fb3c8b93f99fa7a7ab60d1b6
andrektozzi <andrektozzi@gmail.com>
2022-08-31 09:30:51 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { json } from 'express';
+import cors from 'cors';
+
+
+const app = express();
+app.use(json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+});
